d2b5f53c2038f732124639dae2e787b6a9452890
toMilanesi <tomi.milanesi@gmail.com>
2022-07-08 14:53:26 -0300

    Add react and babel dependencies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles/main.scss';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Hello World</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.?js$/,
+        exclude: /node.modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        },
+      },
+      {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'sass-loader'],
       },
